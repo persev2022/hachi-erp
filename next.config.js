ad6993d30ec337427830a21cfc43a2373a7b2d67
Victor A Lapegna <victoralapegna@Victors-MacBook-Air-3.local>
2026-07-01 21:24:47 -0300

    fix: static export for Vercel free tier

--- next.config.js
+++ next.config.js
@@ -1,25 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  headers: async () => [
-    {
-      source: "/(.*)",
-      headers: [
-        { key: "X-Frame-Options", value: "DENY" },
-        { key: "X-Content-Type-Options", value: "nosniff" },
-        { key: "Referrer-Policy", value: "strict-origin-when-cross-origin" },
-        { key: "X-XSS-Protection", value: "1; mode=block" },
-        {
-          key: "Strict-Transport-Security",
-          value: "max-age=31536000; includeSubDomains",
-        },
-        {
-          key: "Content-Security-Policy",
-          value:
-            "default-src 'self'; script-src 'self' 'unsafe-eval' 'unsafe-inline'; style-src 'self' 'unsafe-inline'; img-src 'self' data: blob:; font-src 'self'; connect-src 'self' https://backend.botconversa.com.br;",
-        },
-      ],
-    },
-  ],
+  output: "export",
+  images: { unoptimized: true },
 };
 
 module.exports = nextConfig;
